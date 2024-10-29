b0feb34674272bcdfaf7607c7e00e591a40291e2
Khursheed Hassan <khassan@achieveon.com>
2024-10-29 13:29:31 -0700

    more text and visibilty fixes

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -3,7 +3,12 @@ import HistoryContainer from './history-container'
 export async function Sidebar() {
   return (
     <div className="h-screen p-2 fixed top-0 right-0 flex-col justify-center pb-24 hidden sm:flex">
-      <HistoryContainer location="sidebar" />
+      <HistoryContainer
+        location="sidebar"
+        onConversationSelect={function (conversationId: string): void {
+          throw new Error('Function not implemented.')
+        }}
+      />
     </div>
   )
 }
